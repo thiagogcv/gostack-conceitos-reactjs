65d825750fce08c42ad0477921991dff843d771a
Thiago Costa <thiagogcv@gmail.com>
2020-07-17 23:19:55 -0300

    Configurando webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        path: path.resolve(__dirname,),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    }
+}
