304b567386894691610956ba2a6d1ffe00a34104
Thiago Costa <thiagogcv@gmail.com>
2020-07-18 01:38:57 -0300

    Add pendência webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        path: path.resolve(__dirname,),
+        path: path.resolve(__dirname,'public'),
         filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
     },
     module: {
         rules: [
